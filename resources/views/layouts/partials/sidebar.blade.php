c8b125e946426fd4bf118317f095f9c67a8d7e60
farid-maulana <faridmaulana1921@gmail.com>
2021-09-30 23:52:54 +0700

    Setup front end tipe kamar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -34,6 +34,14 @@
           </a>
         </li>
         <li class="nav-item">
+          <a class="nav-link {{ (request()->segment(1) == 'roomTypes') ? 'active' : '' }}" href="{{ route('roomTypes.index') }}">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="fas fa-door-open {{ (request()->segment(1) != 'roomTypes') ? 'text-dark' : '' }}" style="font-size: 13px; top: 0px !important"></i>
+            </div>
+            <span class="nav-link-text ms-1">Tipe Kamar</span>
+          </a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link {{ (request()->segment(1) == 'rooms') ? 'active' : '' }}" href="{{ route('rooms.index') }}">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="fas fa-key {{ (request()->segment(1) != 'rooms') ? 'text-dark' : '' }}" style="font-size: 13px; top: 0px !important"></i>
