dbab3bf11e0d8fe918a13238096a4732614c6575
farid-maulana <faridmaulana1921@gmail.com>
2021-10-04 16:49:12 +0700

    Setup front end manajemen customer

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -50,6 +50,14 @@
           </a>
         </li>
         <li class="nav-item">
+          <a class="nav-link {{ (request()->segment(1) == 'customers') ? 'active' : '' }}" href="{{ route('customers.index') }}">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="fas fa-users {{ (request()->segment(1) != 'customers') ? 'text-dark' : '' }}" style="font-size: 13px; top: 0px !important"></i>
+            </div>
+            <span class="nav-link-text ms-1">Manajemen Customer</span>
+          </a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link " href="#">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="fas fa-credit-card text-dark" style="font-size: 13px; top: 0px !important"></i>
