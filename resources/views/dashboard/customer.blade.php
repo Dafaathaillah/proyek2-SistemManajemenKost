739f75d11ccd4d84381ce6bcd99a2dd5740acf21
farid-maulana <faridmaulana1921@gmail.com>
2021-10-13 00:03:29 +0700

    Fix dashboard customer

--- resources/views/dashboard/customer.blade.php
+++ resources/views/dashboard/customer.blade.php
@@ -2,160 +2,75 @@
 
 @section('content')
 <div class="row">
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-8">
-                        <div class="numbers">
-                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Money</p>
-                            <h5 class="font-weight-bolder mb-0">
-                                $53,000
-                                <span class="text-success text-sm font-weight-bolder">+55%</span>
-                            </h5>
-                        </div>
-                    </div>
-                    <div class="col-4 text-end">
-                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                            <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-8">
-                        <div class="numbers">
-                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Users</p>
-                            <h5 class="font-weight-bolder mb-0">
-                                2,300
-                                <span class="text-success text-sm font-weight-bolder">+3%</span>
-                            </h5>
-                        </div>
-                    </div>
-                    <div class="col-4 text-end">
-                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                            <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-8">
-                        <div class="numbers">
-                            <p class="text-sm mb-0 text-capitalize font-weight-bold">New Clients</p>
-                            <h5 class="font-weight-bolder mb-0">
-                                +3,462
-                                <span class="text-danger text-sm font-weight-bolder">-2%</span>
-                            </h5>
-                        </div>
-                    </div>
-                    <div class="col-4 text-end">
-                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                            <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card">
-            <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-8">
-                        <div class="numbers">
-                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Sales</p>
-                            <h5 class="font-weight-bolder mb-0">
-                                $103,430
-                                <span class="text-success text-sm font-weight-bolder">+5%</span>
-                            </h5>
-                        </div>
-                    </div>
-                    <div class="col-4 text-end">
-                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                            <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="row mt-4">
     @foreach($boardinghouses as $bourdinghouse)
-    <div class="col-lg-6 mb-lg-0 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="d-flex flex-column h-100">
-                            <p class="mb-1 pt-2 text-bold">Bougenville Kost</p>
-                            <h5 class="font-weight-bolder">Aturan</h5>
-                            <ul class="mb-4">
-                                <li> {!! $bourdinghouse->rule !!}  </li>
-                            </ul>
+        <div class="col-lg-6 mb-lg-0 mb-4">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="d-flex flex-column h-100">
+                                <p class="mb-1 pt-2 text-bold">Bougenville Kost</p>
+                                <h5 class="font-weight-bolder">Aturan</h5>
+                                <div class="mb-2">
+                                    {!! $bourdinghouse->rule !!}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     @endforeach
     @foreach($customers as $customer)
-    @if ($customer->user->id == Auth::user()->id)
-    <div class="col-md-6 mt-md-0 mt-4">
-        <div class="card">
-            <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="d-flex flex-column h-100">
-                            <p class="mb-1 pt-2 text-bold">Informasi Kamar Anda</p>
-                            <h5 class="font-weight-bolder">{{ $customer->room->name }}</h5>
-                            <dl class="row">
-                                <dt class="col-sm-4">Tipe Kamar</dt>
-                                <dd class="col-sm-8">{{ $customer->room->roomtype->name }}</dd>
+        @if($customer->user->id == Auth::user()->id)
+            <div class="col-lg-6 mb-lg-0 mt-md-0 mt-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="d-flex flex-column h-100">
+                                    <p class="mb-1 pt-2 text-bold">Informasi Kamar Anda</p>
+                                    <h5 class="font-weight-bolder">{{ $customer->room->name }}</h5>
+                                    <dl class="row">
+                                        <dt class="col-sm-4">Tipe Kamar</dt>
+                                        <dd class="col-sm-8">{{ $customer->room->roomtype->name }}</dd>
 
-                                <dt class="col-sm-4">Fasilitas</dt>
-                                <dd class="col-sm-8"> - -- -</dd>
-                                
-                                <dt class="col-sm-4">Harga</dt>
-                                <dd class="col-sm-8">Rp {{ number_format($customer->room->price,0,',','.') }}</dd>
-                                
-                            </dl>
-                            <hr class="mb-5">
-                            <dl class="row">
-                                <dt class="col-sm-4">Nama Penyewa</dt>
-                                <dd class="col-sm-8">{{ Auth::user()->name }}</dd>
+                                        <dt class="col-sm-4">Fasilitas</dt>
+                                        <dd class="col-sm-8">
+                                            @foreach ($customer->room->roomtype->facility as $facility)
+                                                {{ $facility->name . ($loop->last ? '.' : ', ') }}
+                                            @endforeach
+                                        </dd>
 
-                                <dt class="col-sm-4">Jenis Kelamin</dt>
-                                <dd class="col-sm-8">
-                                @if($customer->gender == 'L')
-                                    <div class="col-sm-8">Laki Laki</div>
-                                @else($customer->gender == 'P')
-                                <div class="col-sm-8">Perempuan</div>
-                                @endif
-                                </dd>
+                                        <dt class="col-sm-4">Harga</dt>
+                                        <dd class="col-sm-8">Rp
+                                            {{ number_format($customer->room->roomType->price,0,',','.') }}
+                                        </dd>
+                                    </dl>
+                                    <hr class="mb-3 mt-0">
+                                    <dl class="row">
+                                        <dt class="col-sm-4">Nama Penyewa</dt>
+                                        <dd class="col-sm-8">{{ Auth::user()->name }}</dd>
 
-                                <dt class="col-sm-4">Email Akun</dt>
-                                <dd class="col-sm-8">{{ Auth::user()->email }}</dd>
-                            </dl>
+                                        <dt class="col-sm-4">Jenis Kelamin</dt>
+                                        <dd class="col-sm-8">
+                                            @if($customer->gender == 'L')
+                                                <div class="col-sm-8">Laki Laki</div>
+                                            @else
+                                                <div class="col-sm-8">Perempuan</div>
+                                            @endif
+                                        </dd>
+
+                                        <dt class="col-sm-4">Email Akun</dt>
+                                        <dd class="col-sm-8">{{ Auth::user()->email }}</dd>
+                                    </dl>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    @endif
+        @endif
     @endforeach
 </div>
 <div class="row mt-4">
